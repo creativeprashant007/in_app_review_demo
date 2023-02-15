fcd8187ba6cad29a768b4912f0ac017ca7ddbfe0
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-02-15 16:58:57 +0545

    feature:in app update feature added

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:in_app_review/in_app_review.dart';
+
+enum Availability { loading, available, unavailable }
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
+  final InAppReview _inAppReview = InAppReview.instance;
+
+  String appStoreId = '';
+  String microsoftStoreId = '';
+  Availability availability = Availability.loading;
   @override
   void onInit() {
+    requestReview();
+    (<T>(T? o) => o!)(WidgetsBinding.instance).addPostFrameCallback((_) async {
+      try {
+        final isAvailable = await _inAppReview.isAvailable();
+
+        availability = isAvailable && !Platform.isAndroid
+            ? Availability.available
+            : Availability.unavailable;
+      } catch (_) {
+        availability = Availability.unavailable;
+      }
+    });
+    update();
     super.onInit();
   }
 
@@ -20,4 +44,14 @@ class HomeController extends GetxController {
   }
 
   void increment() => count.value++;
+  void setAppStoreId(String id) => appStoreId = id;
+
+  void setMicrosoftStoreId(String id) => microsoftStoreId = id;
+
+  Future<void> requestReview() => _inAppReview.requestReview();
+
+  Future<void> openStoreListing() => _inAppReview.openStoreListing(
+        appStoreId: appStoreId,
+        microsoftStoreId: microsoftStoreId,
+      );
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,15 +8,31 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return MaterialApp(
+      title: 'In App Review Example',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('In App Review Example')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('In App Review status: ${controller.availability.name}'),
+            TextField(
+              onChanged: controller.setAppStoreId,
+              decoration: const InputDecoration(hintText: 'App Store ID'),
+            ),
+            TextField(
+              onChanged: controller.setMicrosoftStoreId,
+              decoration: const InputDecoration(hintText: 'Microsoft Store ID'),
+            ),
+            ElevatedButton(
+              onPressed: controller.requestReview,
+              child: const Text('Request Review'),
+            ),
+            ElevatedButton(
+              onPressed: controller.openStoreListing,
+              child: const Text('Open Store Listing'),
+            ),
+          ],
         ),
       ),
     );
